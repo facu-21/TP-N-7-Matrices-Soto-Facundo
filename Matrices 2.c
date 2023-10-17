94ba476f9cc43e7c03f88d59510a7fc5e4cc87ca
Gustavo Gatamora <gustavo.gatamora@gmail.com>
2023-10-17 10:05:34 -0300

    Devolución. -
    ¡Muy buen trabajo!

--- Matrices 2.c
+++ Matrices 2.c
@@ -10,10 +10,10 @@ int main() {
 		}
 	}
 	for(j=0;j<=2;j++){
-		m[0][j]=m[0][j]*4;
+		m[0][j]=m[0][j]*4;//Condición.
 	}
 	for(i=0;i<=1;i++){
-		m[i][2]=m[i][2]*3;
+		m[i][2]=m[i][2]*3;//Condición.
 	}
 	printf("\n");
 	printf("Matriz modificada:\n");
